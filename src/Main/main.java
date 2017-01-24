98d5555ff1e38d7450071c761e61a09a9e7d8687
liinala <liina.laumets@itcollege.ee>
2017-01-24 14:37:00 +0200

    Update main.java

--- src/Main/main.java
+++ src/Main/main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Alusta Pommitamist");
-        System.out.println("Autor Liina")
+        System.out.println("Autor Liina");
+        System.out.println("Aasta 2017 ");
     }
 }
